299062b28922079d0c4f7786c483e5852fcbc612
Carlos Alcedo <carlosalcedojavier@gmail.com>
2026-03-09 11:12:54 -0500

    ajust project for apk 5

--- wapp/urls.py
+++ wapp/urls.py
@@ -3,8 +3,11 @@ from django.contrib import admin
 from django.urls import path, include
 from apps.crm.views import lead_stats_api, CreateLeadCustomView
 from apps.client.views import get_company_by_ruc
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
+    path('', RedirectView.as_base(url='/admin/'), name='home'),
     path('admin/', admin.site.urls),
     path('api/lead-stats/', lead_stats_api, name='lead_stats_api'),
     path('api/get-company/', get_company_by_ruc, name='get_company_by_ruc'),
