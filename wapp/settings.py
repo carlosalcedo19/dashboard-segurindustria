d63fcb22943d0255aa8d2d3c7ba5f824134f7af4
Carlos Alcedo <carlosalcedojavier@gmail.com>
2026-03-09 10:57:09 -0500

    ajust project for apk 3

--- wapp/settings.py
+++ wapp/settings.py
@@ -161,6 +161,9 @@ UNFOLD = {
                 "950": "17 29 77"
             }
     },
+    "STYLES": [
+        lambda request: "https://dashboard-segurindustria.onrender.com/manifest.json", 
+    ],
     
     "SIDEBAR": {
     "show_search": False,
